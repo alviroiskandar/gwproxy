alviroiskandar/gwproxy

commit 9d5b6f3ecc6494af189c7237291c2fee6dd1b312

epoll: Extract socks5-connect reply prep function
A preparation patch to integrate socks5 with io_uring. The io_uring
part will call the same function.

Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/gwproxy/gwproxy.c
@@ -1158,6 +1158,88 @@ int gwp_create_timer(int fd, int sec, int nsec)
 	return fd;
 }
 
+static int socks5_translate_err(int err)
+{
+	switch (err) {
+	case 0:
+		return GWP_SOCKS5_REP_SUCCESS;
+	case -EPERM:
+	case -EACCES:
+		return GWP_SOCKS5_REP_NOT_ALLOWED;
+	case -ENETUNREACH:
+		return GWP_SOCKS5_REP_NETWORK_UNREACHABLE;
+	case -EHOSTUNREACH:
+		return GWP_SOCKS5_REP_HOST_UNREACHABLE;
+	case -ECONNREFUSED:
+		return GWP_SOCKS5_REP_CONN_REFUSED;
+	case -ETIMEDOUT:
+		return GWP_SOCKS5_REP_TTL_EXPIRED;
+	default:
+		return GWP_SOCKS5_REP_FAILURE;
+	}
+}
+
+static int get_local_addr_for_socks5(struct gwp_ctx *ctx, int fd,
+				     struct gwp_socks5_addr *ba)
+{
+	struct gwp_sockaddr t;
+	socklen_t len = sizeof(t);
+	int r;
+
+	r = __sys_getsockname(fd, &t.sa, &len);
+	if (r < 0) {
+		pr_err(&ctx->lh, "getsockname error: %s", strerror(-r));
+		return r;
+	}
+
+	switch (t.sa.sa_family) {
+	case AF_INET:
+		ba->ver = GWP_SOCKS5_ATYP_IPV4;
+		memcpy(&ba->ip4, &t.i4.sin_addr, 4);
+		ba->port = ntohs(t.i4.sin_port);
+		return 0;
+	case AF_INET6:
+		ba->ver = GWP_SOCKS5_ATYP_IPV6;
+		memcpy(&ba->ip6, &t.i6.sin6_addr, 16);
+		ba->port = ntohs(t.i6.sin6_port);
+		return 0;
+	default:
+		pr_err(&ctx->lh, "Unsupported address family %d for local socket",
+			t.sa.sa_family);
+		return -EAFNOSUPPORT;
+	}
+}
+
+__hot
+int gwp_socks5_prep_connect_reply(struct gwp_wrk *w, struct gwp_conn_pair *gcp,
+				  int err)
+{
+	struct gwp_socks5_conn *sc = gcp->s5_conn;
+	struct gwp_socks5_addr ba;
+	size_t out_len;
+	void *out;
+	int r;
+
+	if (err == 0) {
+		r = get_local_addr_for_socks5(w->ctx, gcp->target.fd, &ba);
+		if (unlikely(r))
+			return r;
+	} else {
+		memset(&ba, 0, sizeof(ba));
+		ba.ver = GWP_SOCKS5_ATYP_IPV4;
+	}
+
+	err = socks5_translate_err(err);
+	out = gcp->target.buf + gcp->target.len;
+	out_len = gcp->target.cap - gcp->target.len;
+	r = gwp_socks5_conn_cmd_connect_res(sc, &ba, err, out, &out_len);
+	if (r < 0)
+		return r;
+
+	gcp->target.len += out_len;
+	return 0;
+}
+
 noinline
 static void *gwp_ctx_thread_entry(void *arg)
 {

src/gwproxy/ev/epoll.c
@@ -542,92 +542,22 @@ static int do_splice(struct gwp_conn *src, struct gwp_conn *dst, bool do_recv,
 	return 0;
 }
 
-static int get_local_addr(struct gwp_ctx *ctx, int fd,
-			  struct gwp_socks5_addr *ba)
-{
-	struct gwp_sockaddr t;
-	socklen_t len = sizeof(t);
-	int r;
-
-	r = __sys_getsockname(fd, &t.sa, &len);
-	if (r < 0) {
-		pr_err(&ctx->lh, "getsockname error: %s", strerror(-r));
-		return r;
-	}
-
-	switch (t.sa.sa_family) {
-	case AF_INET:
-		ba->ver = GWP_SOCKS5_ATYP_IPV4;
-		memcpy(&ba->ip4, &t.i4.sin_addr, 4);
-		ba->port = ntohs(t.i4.sin_port);
-		return 0;
-	case AF_INET6:
-		ba->ver = GWP_SOCKS5_ATYP_IPV6;
-		memcpy(&ba->ip6, &t.i6.sin6_addr, 16);
-		ba->port = ntohs(t.i6.sin6_port);
-		return 0;
-	default:
-		pr_err(&ctx->lh, "Unsupported address family %d for local socket",
-			t.sa.sa_family);
-		return -EAFNOSUPPORT;
-	}
-}
-
-static int socks5_translate_err(int err)
-{
-	switch (err) {
-	case 0:
-		return GWP_SOCKS5_REP_SUCCESS;
-	case -EPERM:
-	case -EACCES:
-		return GWP_SOCKS5_REP_NOT_ALLOWED;
-	case -ENETUNREACH:
-		return GWP_SOCKS5_REP_NETWORK_UNREACHABLE;
-	case -EHOSTUNREACH:
-		return GWP_SOCKS5_REP_HOST_UNREACHABLE;
-	case -ECONNREFUSED:
-		return GWP_SOCKS5_REP_CONN_REFUSED;
-	case -ETIMEDOUT:
-		return GWP_SOCKS5_REP_TTL_EXPIRED;
-	default:
-		return GWP_SOCKS5_REP_FAILURE;
-	}
-}
-
 __hot
 static int prep_and_send_socks5_rep_connect(struct gwp_wrk *w,
 					    struct gwp_conn_pair *gcp,
 					    int err)
 {
-	struct gwp_socks5_conn *sc = gcp->s5_conn;
-	struct gwp_socks5_addr ba;
-	size_t out_len;
 	ssize_t sr;
-	void *out;
 	int r;
 
-	if (err == 0) {
-		r = get_local_addr(w->ctx, gcp->target.fd, &ba);
-		if (unlikely(r))
-			return r;
-	} else {
-		memset(&ba, 0, sizeof(ba));
-		ba.ver = GWP_SOCKS5_ATYP_IPV4;
+	r = gwp_socks5_prep_connect_reply(w, gcp, err);
+	if (gcp->target.len) {
+		sr = __do_send(&gcp->target, &gcp->client);
+		if (unlikely(sr < 0))
+			return sr;
 	}
 
-	err = socks5_translate_err(err);
-	out = gcp->target.buf + gcp->target.len;
-	out_len = gcp->target.cap - gcp->target.len;
-	r = gwp_socks5_conn_cmd_connect_res(sc, &ba, err, out, &out_len);
-	if (r < 0)
-		return r;
-
-	gcp->target.len += out_len;
-	sr = __do_send(&gcp->target, &gcp->client);
-	if (unlikely(sr < 0))
-		return sr;
-
-	return 0;
+	return r;
 }
 
 __hot

src/gwproxy/gwproxy.h
@@ -211,4 +211,7 @@ void log_conn_pair_created(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
 		ip_to_str(&gcp->client_addr), ip_to_str(&gcp->target_addr));
 }
 
+int gwp_socks5_prep_connect_reply(struct gwp_wrk *w, struct gwp_conn_pair *gcp,
+				  int err);
+
 #endif /* #ifndef GWPROXY_H */

commit ce084920f832892ff0392f1c43ce541077848e10

socks5: Fix BND.PORT byte order in the command reply
get_local_addr_for_socks5() stored the bound port as ntohs(sin_port),
i.e. host byte order, into struct gwp_socks5_addr.port, which is __be16
(network order) and is serialized to the wire verbatim by
gwp_socks5_addr_encode(). Every other site treats the field as raw
network order (e.g. gwp_socks5_prepare_target_addr() assigns
sin_port = dst->port directly), so on a little-endian host the reply's
BND.PORT came out byte-swapped. This was invisible for CONNECT because
clients ignore BND.PORT, but UDP ASSOCIATE clients must send their
datagrams to exactly that port. Store the raw network-order port.

Fixes: 9d5b6f3ecc64 ("epoll: Extract socks5-connect reply prep function")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

src/gwproxy/gwproxy.c
@@ -1722,12 +1722,12 @@ static int get_local_addr_for_socks5(struct gwp_ctx *ctx, int fd,
 	case AF_INET:
 		ba->ver = GWP_SOCKS5_ATYP_IPV4;
 		memcpy(&ba->ip4, &t.i4.sin_addr, 4);
-		ba->port = ntohs(t.i4.sin_port);
+		ba->port = t.i4.sin_port;
 		return 0;
 	case AF_INET6:
 		ba->ver = GWP_SOCKS5_ATYP_IPV6;
 		memcpy(&ba->ip6, &t.i6.sin6_addr, 16);
-		ba->port = ntohs(t.i6.sin6_port);
+		ba->port = t.i6.sin6_port;
 		return 0;
 	default:
 		pr_err(&ctx->lh, "Unsupported address family %d for local socket",