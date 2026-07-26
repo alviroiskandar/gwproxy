alviroiskandar/gwproxy

commit e1631ac52a76e1a685cace950ca0cceeff2ad98b

io_uring: Integrate socks5 to io_uring
The first working socks5 proxy using io_uring as event loop.

Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/gwproxy/ev/io_uring.c
@@ -169,14 +169,13 @@ static bool put_gcp(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
 	cl_fd = gcp->client.fd;
 	gcp->flags |= GWP_CONN_FLAG_NO_CLOSE_FD;
 	gwp_free_conn_pair(w, gcp);
-	prep_close(w, tg_fd);
-	prep_close(w, cl_fd);
-	return true;
-}
 
-static void kill_gcp(struct gwp_conn_pair *gcp)
-{
-	gcp->flags |= GWP_CONN_FLAG_IS_DYING;
+	if (tg_fd >= 0)
+		prep_close(w, tg_fd);
+	if (cl_fd >= 0)
+		prep_close(w, cl_fd);
+
+	return true;
 }
 
 static struct io_uring_sqe *prep_connect_target(struct gwp_wrk *w,
@@ -265,8 +264,8 @@ static struct io_uring_sqe *prep_send_target(struct gwp_wrk *w,
 	return s;
 }
 
-static struct io_uring_sqe *prep_send_client(struct gwp_wrk *w,
-					     struct gwp_conn_pair *gcp)
+static struct io_uring_sqe *__prep_send_client(struct gwp_wrk *w,
+					       struct gwp_conn_pair *gcp)
 {
 	size_t len = gcp->target.len;
 	char *buf = gcp->target.buf;
@@ -280,7 +279,6 @@ static struct io_uring_sqe *prep_send_client(struct gwp_wrk *w,
 	io_uring_prep_send(s, fd, buf, len, MSG_NOSIGNAL);
 #endif
 	io_uring_sqe_set_data(s, gcp);
-	s->user_data |= EV_BIT_CLIENT_SEND;
 	get_gcp(gcp);
 	pr_dbg(&w->ctx->lh,
 		"Prepared send for client fd=%d, len=%zu, buf=%p, ref_cnt=%d",
@@ -288,14 +286,31 @@ static struct io_uring_sqe *prep_send_client(struct gwp_wrk *w,
 	return s;
 }
 
+static struct io_uring_sqe *prep_send_client(struct gwp_wrk *w,
+					     struct gwp_conn_pair *gcp)
+{
+	struct io_uring_sqe *s = __prep_send_client(w, gcp);
+	s->user_data |= EV_BIT_CLIENT_SEND;
+	return s;
+}
+
+static struct io_uring_sqe *prep_send_client_no_cb(struct gwp_wrk *w,
+						   struct gwp_conn_pair *gcp)
+{
+	struct io_uring_sqe *s = __prep_send_client(w, gcp);
+	s->user_data |= EV_BIT_CLIENT_SEND_NO_CB;
+	return s;
+}
+
 static struct io_uring_sqe *prep_timer_target(struct gwp_wrk *w,
-					      struct gwp_conn_pair *gcp)
+					      struct gwp_conn_pair *gcp,
+					      int sec)
 {
 	struct io_uring_sqe *s = get_sqe_nofail(w);
 
 	gcp->ts.tv_nsec = 0;
-	gcp->ts.tv_sec = w->ctx->cfg.connect_timeout;
-	io_uring_prep_timeout(s, &gcp->ts, 1, 0);
+	gcp->ts.tv_sec = sec;
+	io_uring_prep_timeout(s, &gcp->ts, 0, 0);
 	io_uring_sqe_set_data(s, gcp);
 	s->user_data |= EV_BIT_TIMER;
 	get_gcp(gcp);
@@ -347,8 +362,46 @@ static void shutdown_gcp(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
 	gcp->flags |= GWP_CONN_FLAG_IS_CANCEL;
 }
 
-static int arm_gcp(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
+static struct io_uring_sqe *prep_recv_client_socks5(struct gwp_wrk *w,
+						    struct gwp_conn_pair *gcp)
 {
+	struct io_uring_sqe *s = prep_recv_client(w, gcp);
+	s->user_data &= ~EV_BIT_ALL;
+	s->user_data |= EV_BIT_CLIENT_SOCKS5;
+	return s;
+}
+
+static int arm_gcp_socks5(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
+{
+	struct gwp_ctx *ctx = w->ctx;
+	int r;
+
+	gcp->s5_conn = gwp_socks5_conn_alloc(ctx->socks5);
+	if (unlikely(!gcp->s5_conn))
+		return -ENOMEM;
+
+	r = prep_nr_sqes(w, 4);
+	if (unlikely(r < 0)) {
+		pr_err(&w->ctx->lh, "Failed to prepare sqes for connection pair");
+		return r;
+	}
+
+	prep_recv_client_socks5(w, gcp);
+
+	/*
+	 * If we are running as a SOCKS5 proxy, the initial connection
+	 * does not have a target socket. We will create the target
+	 * socket later, when the client sends a CONNECT command.
+	 */
+	if (ctx->cfg.socks5_timeout > 0)
+		prep_timer_target(w, gcp, ctx->cfg.socks5_timeout);
+
+	return 0;
+}
+
+static int do_prep_connect(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
+{
+	struct gwp_ctx *ctx = w->ctx;
 	struct io_uring_sqe *s;
 	int r;
 
@@ -362,9 +415,26 @@ static int arm_gcp(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
 	s->flags |= IOSQE_IO_LINK;
 	prep_recv_client(w, gcp);
 	prep_recv_target(w, gcp);
-	prep_timer_target(w, gcp);
+
+	if (ctx->cfg.connect_timeout > 0)
+		prep_timer_target(w, gcp, ctx->cfg.connect_timeout);
 
 	return 0;
+}
+
+static int arm_gcp_no_socks5(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
+{
+	return do_prep_connect(w, gcp);
+}
+
+static int arm_gcp(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
+{
+	struct gwp_ctx *ctx = w->ctx;
+
+	if (ctx->cfg.as_socks5)
+		return arm_gcp_socks5(w, gcp);
+	else
+		return arm_gcp_no_socks5(w, gcp);
 }
 
 static int __handle_ev_accept(struct gwp_wrk *w, struct io_uring_cqe *cqe)
@@ -384,10 +454,14 @@ static int __handle_ev_accept(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 		return fd;
 	}
 
-	tg_fd = gwp_create_sock_target(w, &ctx->target_addr, NULL, false);
-	if (unlikely(tg_fd < 0)) {
-		pr_err(&ctx->lh, "Create target socket: %s", strerror(-tg_fd));
-		goto out_close;
+	if (!ctx->cfg.as_socks5) {
+		tg_fd = gwp_create_sock_target(w, &ctx->target_addr, NULL, false);
+		if (unlikely(tg_fd < 0)) {
+			pr_err(&ctx->lh, "Create target socket: %s", strerror(-tg_fd));
+			goto out_close;
+		}
+	} else {
+		tg_fd = -1;
 	}
 
 	gcp = gwp_alloc_conn_pair(w);
@@ -414,9 +488,11 @@ out_free_pair:
 	gcp->client.fd = gcp->target.fd = gcp->timer_fd = -1;
 	gwp_free_conn_pair(w, gcp);
 out_close_tg_fd:
-	prep_close(w, tg_fd);
+	if (tg_fd >= 0)
+		prep_close(w, tg_fd);
 out_close:
-	prep_close(w, fd);
+	if (fd >= 0)
+		prep_close(w, fd);
 	return -ENOMEM;
 }
 
@@ -436,11 +512,11 @@ static int handle_ev_accept(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 static int handle_ev_target_connect(struct gwp_wrk *w, void *udata, int res)
 {
 	struct gwp_conn_pair *gcp = udata;
+	int r;
 
 	if (unlikely(res < 0)) {
 		pr_err(&w->ctx->lh, "Target connect failed: %s", strerror(-res));
-		kill_gcp(gcp);
-		return 0;
+		return res;
 	}
 
 	prep_timer_del_target(w, gcp);
@@ -451,6 +527,15 @@ static int handle_ev_target_connect(struct gwp_wrk *w, void *udata, int res)
 		ip_to_str(&gcp->client_addr),
 		ip_to_str(&gcp->target_addr));
 
+	if (w->ctx->cfg.as_socks5) {
+		r = gwp_socks5_prep_connect_reply(w, gcp, res);
+		if (r)
+			return r;
+
+		if (gcp->target.len)
+			prep_send_client_no_cb(w, gcp);
+	}
+
 	return 0;
 }
 
@@ -458,10 +543,11 @@ static int handle_ev_timer(struct gwp_wrk *w, struct gwp_conn_pair *gcp,
 			   bool is_timer_del, int res)
 {
 	struct gwp_ctx *ctx = w->ctx;
+	int r = 0;
 
-	if (!gcp->is_target_alive) {
-		kill_gcp(gcp);
+	if (!gcp->is_target_alive && res == -ETIME) {
 		assert(is_timer_del == false);
+		r = -ETIME;
 		pr_warn(&ctx->lh,
 			"Connection timeout! (idx=%u, cfd=%d, tfd=%d, ca=%s, ta=%s)",
 			gcp->idx, gcp->client.fd, gcp->target.fd,
@@ -475,7 +561,7 @@ static int handle_ev_timer(struct gwp_wrk *w, struct gwp_conn_pair *gcp,
 		gcp->timer_fd, ip_to_str(&gcp->client_addr),
 		ip_to_str(&gcp->target_addr), is_timer_del, res);
 
-	return 0;
+	return r;
 }
 
 static int handle_sock_ret(int r)
@@ -543,7 +629,8 @@ static int handle_ev_client_send(struct gwp_wrk *w, struct gwp_conn_pair *gcp,
 	}
 
 	gwp_conn_buf_advance(&gcp->target, (size_t)r);
-	prep_recv_target(w, gcp);
+	if (gcp->target.fd >= 0)
+		prep_recv_target(w, gcp);
 	return 0;
 }
 
@@ -565,6 +652,100 @@ static int handle_ev_target_send(struct gwp_wrk *w, struct gwp_conn_pair *gcp,
 	return 0;
 }
 
+static int handle_socks5_connect_target(struct gwp_wrk *w,
+					struct gwp_conn_pair *gcp)
+{
+	int r;
+
+	r = gwp_create_sock_target(w, &gcp->target_addr, NULL, false);
+	if (r < 0) {
+		pr_err(&w->ctx->lh, "Create target socket: %s", strerror(-r));
+		return r;
+	}
+
+	gcp->target.fd = r;
+	return do_prep_connect(w, gcp);
+}
+
+static int prep_domain_resolution(struct gwp_wrk *w, struct gwp_conn_pair *gcp)
+{
+	struct gwp_dns_entry *gde = gcp->gde;
+	struct gwp_ctx *ctx = w->ctx;
+	struct io_uring_sqe *s;
+
+	assert(gde);
+	s = get_sqe_nofail(w);
+	io_uring_prep_poll_add(s, gde->ev_fd, POLLIN);
+	io_uring_sqe_set_data(s, gcp);
+	s->user_data |= EV_BIT_DNS_QUERY;
+	get_gcp(gcp);
+	pr_dbg(&ctx->lh,
+		"Prepared DNS query for domain '%s' (fd=%d, idx=%u, ref_cnt=%d)",
+		gde->name, gde->ev_fd, gcp->idx, gcp->ref_cnt);
+
+	return 0;
+}
+
+static int handle_ev_client_socks5(struct gwp_wrk *w,
+				   struct gwp_conn_pair *gcp,
+				   struct io_uring_cqe *cqe)
+{
+	int r = cqe->res;
+
+	r = handle_sock_ret(r);
+	if (r < 0) {
+		return r;
+	} else if (!r) {
+		prep_recv_client_socks5(w, gcp);
+		return 0;
+	}
+
+	gcp->client.len += (uint32_t)r;
+	r = gwp_socks5_handle_data(gcp);
+	if (r)
+		return r;
+
+	if (gcp->target.len)
+		prep_send_client(w, gcp);
+
+	if (gcp->s5_conn->state == GWP_SOCKS5_ST_CMD_CONNECT) {
+		r = gwp_socks5_prepare_target_addr(w, gcp);
+		if (r == -EINPROGRESS)
+			return prep_domain_resolution(w, gcp);
+
+		if (r)
+			return r;
+
+		r = handle_socks5_connect_target(w, gcp);
+	} else {
+		prep_recv_client_socks5(w, gcp);
+	}
+
+	return r;
+}
+
+static int handle_ev_dns_query(struct gwp_wrk *w, void *udata)
+{
+	struct gwp_conn_pair *gcp = udata;
+	struct gwp_ctx *ctx = w->ctx;
+	struct gwp_dns_entry *gde = gcp->gde;
+
+	if (gde->res) {
+		pr_info(&ctx->lh, "Failed to resolve domain '%s': %d",
+			gde->name, gde->res);
+		return gde->res;
+	}
+
+	gcp->target_addr = gde->addr;
+	pr_info(&ctx->lh, "Domain '%s' resolved to %s (fd=%d, idx=%u)",
+		gde->name, ip_to_str(&gcp->target_addr), gcp->target.fd,
+		gcp->idx);
+
+	gwp_dns_entry_put(gde);
+	gcp->gde = NULL;
+	return handle_socks5_connect_target(w, gcp);
+}
+
 static int handle_event(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 {
 	void *udata = (void *)CLEAR_EV_BIT(cqe->user_data);
@@ -576,10 +757,10 @@ static int handle_event(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 
 	switch (ev_bit) {
 	case EV_BIT_ACCEPT:
-		pr_dbg(&ctx->lh, "Handling accept event");
+		pr_dbg(&ctx->lh, "Handling accept event: %d", cqe->res);
 		return handle_ev_accept(w, cqe);
 	case EV_BIT_TARGET_CONNECT:
-		pr_dbg(&ctx->lh, "Handling target connect event");
+		pr_dbg(&ctx->lh, "Handling target connect event: %d", cqe->res);
 		r = handle_ev_target_connect(w, udata, cqe->res);
 		break;
 	case EV_BIT_TIMER:
@@ -606,6 +787,14 @@ static int handle_event(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 		pr_dbg(&ctx->lh, "Handling target send event: %d", cqe->res);
 		r = handle_ev_target_send(w, udata, cqe);
 		break;
+	case EV_BIT_CLIENT_SOCKS5:
+		pr_dbg(&ctx->lh, "Handling client SOCKS5 event: %d", cqe->res);
+		r = handle_ev_client_socks5(w, udata, cqe);
+		break;
+	case EV_BIT_CLIENT_SEND_NO_CB:
+		pr_dbg(&ctx->lh, "Handling client send no callback event: %d", cqe->res);
+		r = (cqe->res < 0) ? cqe->res : 0;
+		break;
 	case EV_BIT_TARGET_CANCEL:
 		gcp = udata;
 		pr_dbg(&ctx->lh, "Handling target cancel event: %d", cqe->res);
@@ -617,6 +806,10 @@ static int handle_event(struct gwp_wrk *w, struct io_uring_cqe *cqe)
 		pr_dbg(&ctx->lh, "Handling client cancel event: %d", cqe->res);
 		assert(gcp->flags & GWP_CONN_FLAG_IS_CANCEL);
 		r = 0;
+		break;
+	case EV_BIT_DNS_QUERY:
+		pr_dbg(&ctx->lh, "Handling DNS query event: %d", cqe->res);
+		r = handle_ev_dns_query(w, udata);
 		break;
 	case EV_BIT_MSG_RING:
 		return 0;

src/gwproxy/gwproxy.h
@@ -64,7 +64,9 @@ enum {
 	EV_BIT_CLIENT_SEND	= (15ull << 48ull),
 	EV_BIT_CLIENT_RECV	= EV_BIT_CLIENT,
 	EV_BIT_TARGET_RECV	= EV_BIT_TARGET,
-	EV_BIT_MSG_RING		= (16ull << 48ull)
+	EV_BIT_MSG_RING		= (16ull << 48ull),
+
+	EV_BIT_CLIENT_SEND_NO_CB	= (17ull << 48ull),
 };
 
 

commit 16846dfcc27476b2ac09bbeb685872e890e2785c

ev/io_uring: Answer REP 0x04 when a SOCKS5 name fails to resolve
handle_ev_dns_query() logged the failure and returned the error, which
tears the pair down without replying. The client saw the method
selection succeed and then nothing -- it could not distinguish a name
that does not resolve from a proxy that crashed -- and it waited out
the protocol timeout first. RFC 1928 s6 requires a reply carrying a
non-zero REP. epoll has always sent one.

Queue the SOCKS5 failure reply before returning the error, the way
acl_reject_target() already does in this loop, and release the DNS
entry on the failure path too rather than leaving it pinned until the
pair is freed.

HTTP keeps its current behaviour (no error response on either loop);
that gap is shared by both loops, not a divergence, so it is left for
a separate change.

t/0022 now probes an RFC 2606 .invalid name on both loops and asserts
REP 0x04; it reports CLOSED without this fix. socks5_probe.py is a new
raw SOCKS5 client for asserting on reply codes, which curl cannot show.

Fixes: e1631ac52a76 ("io_uring: Integrate socks5 to io_uring")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

src/gwproxy/ev/io_uring.c
@@ -1700,9 +1700,23 @@ static int handle_ev_dns_query(struct gwp_wrk *w, void *udata)
 	struct gwp_dns_entry *gde = gcp->gde;
 
 	if (gde->res) {
+		int res = gde->res;
+
 		pr_info(&ctx->lh, "Failed to resolve domain '%s': %d",
-			gde->name, gde->res);
-		return gde->res;
+			gde->name, res);
+		gwp_dns_entry_put(gde);
+		gcp->gde = NULL;
+
+		/*
+		 * RFC 1928 s6 wants a reply before we hang up; without one the
+		 * client cannot tell a name that does not resolve from a proxy
+		 * that died. The send is queued before the error return tears
+		 * the pair down, exactly as acl_reject_target() does.
+		 */
+		if (gcp->prot_type == GWP_PROT_TYPE_SOCKS5 &&
+		    !gwp_socks5_prep_connect_reply(w, gcp, res))
+			prep_send_client(w, gcp);
+		return res;
 	}
 
 	gcp->target_addr = gde->addr;

t/0022-socks5-domain.sh
@@ -4,6 +4,9 @@
 # SOCKS5 with a hostname target: point curl at "localhost" (an ATYP=DOMAINNAME
 # request) so gwproxy itself must resolve the name before connecting, then
 # verify the fetched payload is byte-exact, on every available event loop.
+# A name that does not resolve must be answered with REP 0x04 (host
+# unreachable) rather than a bare hang-up, so the client can tell a bad name
+# from a broken proxy.
 
 . "$(dirname "$0")/lib.sh"
 require curl
@@ -23,6 +26,14 @@ for loop in epoll io_uring; do
 		|| fail "[$loop] curl via SOCKS5 to hostname target failed"
 	assert_files_equal "$WORK/payload.bin" "$WORK/out.bin" \
 		"[$loop] SOCKS5 domain proxy corrupted the payload"
+
+	# An unresolvable name gets REP 0x04, not silence. ".invalid" is
+	# reserved by RFC 2606 and never resolves.
+	rep="$(python3 "$SERVERS_DIR/socks5_probe.py" --host ::1 --atyp domain \
+		--dst no-such-host.invalid "$pp" 80)"
+	[ "$rep" = "REP=0x04" ] \
+		|| fail "[$loop] unresolvable name got '$rep' (want REP=0x04)"
+
 	kill "$GWP_PID" 2>/dev/null
 done
 

t/servers/socks5_probe.py
@@ -1,0 +1,83 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Raw SOCKS5 client for asserting on the protocol itself, where curl only tells
+# us "it failed". Performs the greeting (optionally RFC 1929 auth), sends one
+# request, and prints exactly what came back:
+#
+#   REP=0xNN   the reply code from a well-formed reply
+#   CLOSED     the proxy hung up without replying
+#   SHORT=n    a truncated reply of n bytes
+#   METHOD=NN  the greeting was answered with an unacceptable method (no request sent)
+#
+# Usage: socks5_probe.py [--host H] [--cmd N] [--atyp ipv4|ipv6|domain]
+#                        [--dst D] [--user U] [--pass P] port dst_port
+#
+# --cmd defaults to 1 (CONNECT); pass 2 for BIND or 3 for UDP ASSOCIATE to
+# check the "command not supported" path.
+import socket, struct, sys
+
+argv = sys.argv[1:]
+
+
+def take_opt(name, default=None):
+    if name in argv:
+        i = argv.index(name)
+        val = argv[i + 1]
+        del argv[i:i + 2]
+        return val
+    return default
+
+
+host = take_opt('--host', '127.0.0.1')
+cmd = int(take_opt('--cmd', '1'))
+atyp = take_opt('--atyp', 'ipv4')
+dst = take_opt('--dst', '127.0.0.1')
+user = take_opt('--user')
+password = take_opt('--pass')
+
+port = int(argv[0])
+dport = int(argv[1])
+
+fam = socket.AF_INET6 if ':' in host else socket.AF_INET
+s = socket.socket(fam, socket.SOCK_STREAM)
+s.settimeout(10)
+s.connect((host, port))
+
+if user is not None:
+    s.sendall(b'\x05\x02\x00\x02')
+else:
+    s.sendall(b'\x05\x01\x00')
+sel = s.recv(2)
+if len(sel) < 2:
+    print('CLOSED')
+    sys.exit(0)
+if sel[1] == 0xff:
+    print('METHOD=%02x' % sel[1])
+    sys.exit(0)
+if sel[1] == 0x02:
+    u8 = user.encode()
+    p8 = (password or '').encode()
+    s.sendall(b'\x01' + bytes([len(u8)]) + u8 + bytes([len(p8)]) + p8)
+    st = s.recv(2)
+    if len(st) < 2 or st[1] != 0:
+        print('AUTHFAIL')
+        sys.exit(0)
+
+if atyp == 'domain':
+    d = dst.encode()
+    addr = b'\x03' + bytes([len(d)]) + d
+elif atyp == 'ipv6':
+    addr = b'\x04' + socket.inet_pton(socket.AF_INET6, dst)
+else:
+    addr = b'\x01' + socket.inet_aton(dst)
+
+s.sendall(bytes([0x05, cmd, 0x00]) + addr + struct.pack('!H', dport))
+
+rep = s.recv(10)
+if not rep:
+    print('CLOSED')
+elif len(rep) < 2:
+    print('SHORT=%d' % len(rep))
+else:
+    print('REP=0x%02x' % rep[1])